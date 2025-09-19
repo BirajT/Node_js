27d3b207ac8693c72c9ed88236db3580300c1e7f
BirajT <birajtamrakar803@gmail.com>
2025-09-19 23:37:39 +0545

    class in module

--- math.js
+++ math.js
@@ -1,3 +1,23 @@
 export function simple(p,t,r){
     return ((p*t*r)/100)
 }
+
+export class Student{
+    name
+    age
+    address
+
+    constructor(name,age,address){
+        this.name=name;
+        this.age=age;
+        this.address=address;
+    }
+
+    display()
+    {
+        console.log("Name = ",this.name);
+        console.log("Age = ",this.age);
+        console.log("Address = ",this.address);
+    }
+    
+}
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-import {simple} from "./math.js"
-
+import {simple,Student} from "./math.js"
 console.log("Simple Interest = ",simple(1,2,100));
+
+let Stu1=new Student("Biraj",20,"Attarkhel")
+let Stu2=new Student("Araj",19,"Bhaktapur")
+Stu1.display()
+Stu2.display()
+
